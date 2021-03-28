74aa31d5916f9a7a76e84ec20aec53231d2e825c
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-03-28 20:18:43 -0300

    Update EstruturaVersificacao.java

--- src/exportacao/EstruturaVersificacao.java
+++ src/exportacao/EstruturaVersificacao.java
@@ -41,7 +41,9 @@ public class EstruturaVersificacao {
     public String getSentecaEscandida() {
         return sentecaEscandida;
     }
-
+    public void setSentecaEscandida(String sentecaEscandida) {
+        this.sentecaEscandida = sentecaEscandida;
+    }
     public boolean isIsUsed() {
         return isUsed;
     }
