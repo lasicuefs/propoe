544b37f89db66eb1206a4d92afa953067afa1f14
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-03-28 20:20:24 -0300

    Update Sentenca.java

--- src/exportacao/Sentenca.java
+++ src/exportacao/Sentenca.java
@@ -7,11 +7,26 @@ public class Sentenca {
 
     @SuppressWarnings("unused")
     private String segmento;
+    private Integer link;
+    private Integer numeroDaFrase;
 
     private List<EstruturaVersificacao> estruturaDeVesificacao;
 
     public String getSegmento() {
         return segmento;
+    }
+
+    public void setSegmento(String segmento) {
+        this.segmento = segmento;
+    }
+    
+    
+    public Integer getLink(){
+        return link;
+    }
+    
+    public Integer getNumeroDaFrase(){
+        return numeroDaFrase;
     }
 
     public List<EstruturaVersificacao> getEstruturaDeVesificacao() {
