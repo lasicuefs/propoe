42bef8bd97463db789c68913780ede41fe681139
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-03-28 20:24:17 -0300

    Update SentencasHelper.java

--- src/javafx/domain/SentencasHelper.java
+++ src/javafx/domain/SentencasHelper.java
@@ -36,6 +36,10 @@ public class SentencasHelper {
         for (Sentenca sentenca : sentencas) {
             //Adiciona na estrutura de versificação a métrica extraida da posição das tônicas.
             //Corrige o bug do arquivo XML que vem com o número de silabas diferente da ultima silaba tônica
+            sentenca.setSegmento(Utils.limparSentencas(sentenca.getSegmento()));
+            sentenca.getEstruturaDeVesificacao().forEach(es -> {
+                es.setSentecaEscandida(Utils.limparSentencas(es.getSentecaEscandida()));
+                
             sentenca.getEstruturaDeVesificacao().forEach(es -> {
                 Metrica metrica = Utils.obterMetricaDoVerso(es.getPosicaoDasTonicas());
                 String palavra = Utils.extrairUltimaPalavraDeSentenca(es.getSentecaEscandida());
