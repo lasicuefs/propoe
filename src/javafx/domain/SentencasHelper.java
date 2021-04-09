425acc3b3ed1564c29c54e34d140fbcbd9bfe137
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-04-09 17:34:36 -0300

    Update SentencasHelper.java

--- src/javafx/domain/SentencasHelper.java
+++ src/javafx/domain/SentencasHelper.java
@@ -11,9 +11,6 @@ import model.UnidadeDeVersificacao;
 
 import java.util.*;
 
-/**
- * @author Ana Cleyge
- */
 public class SentencasHelper {
 
     private final HashMap<String, List<Sentenca>> hp = new HashMap<>();
@@ -26,16 +23,9 @@ public class SentencasHelper {
         return hp;
     }
 
-    /***
-     * Recebe a lista de senteças e preenche o {@link HashMap} hp, calcula a metrica da unidade de versificação.
-     * Define também o segmento da sentença, na estrutura de versificação.
-     *
-     * @param sentencas {@link List<Sentenca>} lista com as sentenças a serem processadas
-     * */
     private void processarSentencas(List<Sentenca> sentencas) {
         for (Sentenca sentenca : sentencas) {
-            //Adiciona na estrutura de versificação a métrica extraida da posição das tônicas.
-            //Corrige o bug do arquivo XML que vem com o número de silabas diferente da ultima silaba tônica
+       
             sentenca.setSegmento(Utils.limparSentencas(sentenca.getSegmento()));
             sentenca.getEstruturaDeVesificacao().forEach(es -> {
                 es.setSentecaEscandida(Utils.limparSentencas(es.getSentecaEscandida()));            
@@ -66,11 +56,6 @@ public class SentencasHelper {
         }
     }
 
-    /**
-     * Metódo resposável por obter as sentenças processadas, organizar em uma string padronizada.
-     *
-     * @return {@link String} uma string com as sentenças à serem exibida na interface.
-     */
     public String mostrarSentencas() {
         StringBuilder sb = new StringBuilder();
         for (String chave : this.hp.keySet()) {
@@ -89,14 +74,6 @@ public class SentencasHelper {
         return sb.toString();
     }
 
-    /**
-     * Este método realiza a contagem de quantas sentenças contem a mesma silaba. Utiliza um HasMap<String, Integer>
-     * A string é a silaba e o integer é a quantidade de vezes que ela foi encontrada. A contagem leva em consideração a
-     * métrica passada por parâmetro.
-     *
-     * @param metrica {@link Metrica} silabas que tiverem a metrica informada por par ametro.
-     * @return hasmap com as silabas e a sua frequência.
-     */
     public Map<String, Integer> getSentencasSilabas(Metrica metrica) {
         Map<String, Integer> silabas = new HashMap<>();
 
@@ -128,20 +105,9 @@ public class SentencasHelper {
         return silabas;
     }
 
-    /**
-     * Percorre o hasmap hp e faz a contagem de quantas estruturas de versificação tem a mesma posição de tônica. A contagem
-     * leva em consideração a métrica passada por parâmetro.
-     * Preenche o HasMap padroesRitimicos com o rítimo e a quantidade encontrada.
-     * Após realizar a contagem, transforma o HasMap em uma Lista com {@link UnidadeDeVersificacao} e calcula a similaridade
-     * do verso.
-     *
-     * @param metrica {@link Metrica} silabas que tiverem a metrica informada por parametro.
-     * @return {@link List<UnidadeDeVersificacao>} lista com as unidades de versificação prontas.
-     */
     public List<UnidadeDeVersificacao> getPadroesRitmicos(Metrica metrica) {
         Map<String, Integer> padroesRitmicos = new HashMap<>();
-
-        //percorre o hashmap e conta quantas estruturas de versificação tem a mesma posição das tonicas
+       
         this.hp.entrySet().stream()
                 .filter(item -> (!(item.getKey().matches("\\d*\\W*"))))
                 .filter(item -> item.getValue().size() > 1)
@@ -154,13 +120,13 @@ public class SentencasHelper {
 
         System.out.println("------------------Padrões Ritmicos de Sentenças------------------");
         System.out.println("Ritmo \t" + "Quantidade \t" + "Similaridade \n");
-        //Cria uma lista com as unidades de versificações para calcular ordenar e calcular a similiaridade
+      
         List<UnidadeDeVersificacao> unidadesDeVersificacoes = new ArrayList<>();
         if (!padroesRitmicos.isEmpty()) {
             padroesRitmicos.forEach((padrao, quantidade) -> unidadesDeVersificacoes.add(new UnidadeDeVersificacao(padrao, quantidade)));
-            //Ordena as unidades pela quantidade
+            
             unidadesDeVersificacoes.sort((o1, o2) -> Integer.compare(o2.getQuantidade(), o1.getQuantidade()));
-            //Calcula a similidade das unidades
+           
             String similar = unidadesDeVersificacoes.get(0).getPadrao();
             unidadesDeVersificacoes
                     .stream()
@@ -173,16 +139,6 @@ public class SentencasHelper {
         return unidadesDeVersificacoes;
     }
 
-    /**
-     * Método que obtem uma lista de silabas disponíveis atraves da analise de silabas do hasmap.
-     *
-     * @param numEstrofes      quantidade de estrofes escolhida pelo usuário.
-     * @param numVersos        quantidade de versos escolhido pelo usuáro.
-     * @param metrica          {@link Metrica} silabas que tiverem a metrica informada por par
-     *                         ametro.
-     * @param usaQuatroSilabas se o usuário escolheu 2 silabas ou 4 silabas para gerar o poema
-     * @return {@link List<String>} uma lista contendo as silabas de satisfazem os parametros
-     */
     public List<String> getSilabasDisponiveis(int numEstrofes, int numVersos, Metrica metrica, boolean usaQuatroSilabas) {
         int qntSilabas = usaQuatroSilabas ? 4 : 2;
         int quantidadeVersos = ((numEstrofes * numVersos) + 1) / qntSilabas;
@@ -204,21 +160,11 @@ public class SentencasHelper {
         return silabas;
     }
 
-    /**
-     * Escolhe uma silaba de uma lista passada por parametro. Caso a silaba informada no parametro selected for Aleatória
-     * será sorteado uma silaba entre as da lista. Caso não seja aleatório, retona o proprio parametro selected.
-     *
-     * @param selected {@link String} com a silaba selecionada.
-     * @param silabas  {@link ObservableList<String>} lista contendo as silabas a serem utilizadas
-     * @return String a silaba escolhida.
-     */
     public String escolherSilaba(ObservableList<String> silabas, String selected) {
         if (selected == null)
             return "";
 
         if (selected.equals("Aleatória")) {
-
-            //TODO: Nova implementação para previnir um infinity lopping, quando existe somente 1 item na lista
             if (silabas.size() <= 2) {
                 return silabas.get(1);
             }
@@ -233,7 +179,6 @@ public class SentencasHelper {
             return silabas.get(randonNum);
         }
 
-        //exibe as sentenças das silabas escolihdas na tela
         System.out.println(String.format("Silaba escolhida: %s", selected));
         this.hp.get(selected).stream().forEach(s -> System.out.println(s.getSegmento()));
         System.out.println("\n\n\n");
@@ -241,9 +186,6 @@ public class SentencasHelper {
         return selected;
     }
 
-    /**
-     * Obtem as metricas contidas nas sentenças
-     */
     public List<Metrica> obterMetricas() {
         Set<Metrica> metricas = new HashSet<>();
         this.hp.forEach((key, sentencas) -> sentencas.forEach(sentenca -> sentenca.getEstruturaDeVesificacao()
