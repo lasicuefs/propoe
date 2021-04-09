b169785c6802f0822db7197685cf59c1f4af6135
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-04-09 17:41:24 -0300

    Update PoemaException.java

--- src/javafx/utilities/PoemaException.java
+++ src/javafx/utilities/PoemaException.java
@@ -1,8 +1,5 @@
 package javafx.utilities;
 
-/**
- * Classe utilizada para lançar uma exception, quando não for possível gerar um poema.
- */
 public class PoemaException extends Exception {
     public PoemaException(String message) {
         super(message, null);
