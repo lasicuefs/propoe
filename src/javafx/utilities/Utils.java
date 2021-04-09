8cb434c22a9cf7a519fe098ad1097ae271108233
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-04-09 17:42:31 -0300

    Update Utils.java

--- src/javafx/utilities/Utils.java
+++ src/javafx/utilities/Utils.java
@@ -14,13 +14,6 @@ public abstract class Utils {
         return new Random();
     }
 
-    /***
-     * Recebe posições das silabas tonicas e utiliza o valor da ultima silaba tônica para definir a metrica
-     *
-     * @param posicaoDasTonicas {@link String} contento a posição das silabas
-     *
-     * @return {@link Metrica} a metrica extraida das posições
-     * */
     public static Metrica obterMetricaDoVerso(String posicaoDasTonicas) {
 
         String[] posicoes = posicaoDasTonicas.split(" ");
@@ -28,13 +21,7 @@ public abstract class Utils {
 
         return Metrica.fromId(posicao);
     }
-
-    /**
-     * Extair um a ultima silaba de uma sentença, seja ela uma palavra ou uma sentença escandida
-     *
-     * @param sentenca sentença
-     * @return String com a silaba
-     */
+    
     public static String extrairUltimaSilabaDeUmaSentenca(String sentenca) {
         if (sentenca == null || sentenca.isEmpty())
             throw new RuntimeException("A sentença nao pode ser nula ou vazia");
@@ -47,12 +34,6 @@ public abstract class Utils {
         return sentenca.substring(start, end);
     }
 
-    /**
-     * Remove qualquer caracter especial do inico ou fim de uma string. Previne a senteças terminadas com (..., /, -, #)
-     *
-     * @param string {@link String} a ser tratada
-     * @return {@link String} com os caracteres especiais removidos
-     */
     public static String removeCharEspecial(String string) {
         if (string == null || string.isEmpty())
             throw new RuntimeException("A string nao pode ser nula ou vazia");
@@ -67,23 +48,11 @@ public abstract class Utils {
         return new StringBuffer(string).reverse().toString();
     }
 
-    /**
-     * Obtem a ultima palavra de uma sentença escândida.
-     *
-     * @return String com a ultima palavra
-     */
     public static String extrairUltimaPalavraDeSentenca(String sentencaEscandida) {
         String[] palavras = sentencaEscandida.trim().split(" "); //quebra a sentenca em palavras
         return palavras[palavras.length - 1]; //obtem a ultima palavra
     }
 
-    /**
-     * Contas a quantidade de tonicas da interesecao de dois posicionamento de tonicas
-     *
-     * @param tonicasA array contendo as posicao de tonicas
-     * @param tonicasB array contendo as posicao de tonicas
-     * @return int numero de tonicas da intersecao
-     */
     public static int contarInteresecaoDeTonicas(String[] tonicasA, String[] tonicasB) {
         return (int) Stream.concat(Arrays.stream(tonicasA), Arrays.stream(tonicasB))
                 .collect(Collectors.groupingBy(strings -> strings, Collectors.counting()))
@@ -100,12 +69,6 @@ public abstract class Utils {
                 .size();
     }
 
-    /**
-     * Obtem a acentuação da palavra
-     *
-     * @param palavra A palavra a ser analisada, a mesma deve vir em forma de sentenca escandida
-     * @return {@link model.Acentuacao} A acentuação da palavra; {@code null} Caso não exista.
-     */
     public static Acentuacao obterAcentuacaoDaPalavra(String palavra) {
         String[] silabas = palavra.split("/"); //quebra a palavra em silabas
 
