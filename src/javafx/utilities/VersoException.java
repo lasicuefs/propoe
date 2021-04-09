9102578b4f636c5dc31211dc983796359deeb7c8
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-04-09 17:42:57 -0300

    Update VersoException.java

--- src/javafx/utilities/VersoException.java
+++ src/javafx/utilities/VersoException.java
@@ -1,8 +1,5 @@
 package javafx.utilities;
 
-/**
- * Classe utilizada para lançar uma exception, quando não for possível gerar um verso.
- */
 public class VersoException extends Exception {
     public VersoException(String message) {
         super(message, null);
