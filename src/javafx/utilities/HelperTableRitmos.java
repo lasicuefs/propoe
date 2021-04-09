7d65c4355dcff34cff2d31835508418e3c49d65d
Ana Cleyge Azevêdo <67861838+AnaCleyge@users.noreply.github.com>
2021-04-09 17:40:07 -0300

    Update HelperTableRitmos.java

--- src/javafx/utilities/HelperTableRitmos.java
+++ src/javafx/utilities/HelperTableRitmos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafx.utilities;
 
 import javafx.beans.property.SimpleIntegerProperty;
@@ -15,9 +10,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.Arrays;
 
-/**
- * @author Ana Cleyge
- */
 public class HelperTableRitmos {
 
     private TableView<ModeloFrequencia> tableViewRitmo;
